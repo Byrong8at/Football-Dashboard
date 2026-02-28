bdc211a673a41d05bdae88b233cc64d35fbdd758
Shown <106871132+Byrong8at@users.noreply.github.com>
2026-02-28 23:38:54 +0200

    launch pipeline

--- project/script_launcher.py
+++ project/script_launcher.py
@@ -3,5 +3,5 @@ from script.League_scrap import Get_League
 from script.Team_scrap import Get_Team
 
 club_list=Get_League()
-annees_a_extraire = [2021,2022,2023,2024,2025]
+annees_a_extraire = [2025]
 Get_Team(club_list, years=annees_a_extraire)
